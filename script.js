b0fc7a8d9d04ca89ceedec026d704a49c851350a
elliot-tomorrow <elliot.clarence.dulay@gmail.com>
2026-02-02 11:48:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirectToPage() {
+    
+    var checkbox = document.getElementById("myCheckbox");
+
+    
+    if (checkbox.checked) {
+        window.location.href = 'captcha2.html';
+    }
+}
+
+
